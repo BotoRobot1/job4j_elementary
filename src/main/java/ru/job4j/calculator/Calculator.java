fae3399d5e6ae72a6d8a85b4b9a6cf716f141467
user <666neoleg999@gmail.com>
2022-06-08 19:36:49 +0700

    Арифметические операции. [#185 #307909]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -4,7 +4,11 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);
     }
 }
